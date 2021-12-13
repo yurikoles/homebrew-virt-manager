993105487ea300ffa794ccc850a8cbd64ede63f2
Yurii Kolesnykov <root@yurikoles.com>
2021-12-13 10:28:05 +0200

    virt-viewer 11.0
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- virt-viewer.rb
+++ virt-viewer.rb
@@ -1,38 +1,39 @@
 class VirtViewer < Formula
   desc "App for virtualized guest interaction"
   homepage "https://virt-manager.org/"
-  url "https://virt-manager.org/download/sources/virt-viewer/virt-viewer-8.0.tar.gz"
-  sha256 "dcf358ed5d7a4900215133135a6492c04311d84332816d930df9a89d6195b6ed"
+  url "https://virt-manager.org/download/sources/virt-viewer/virt-viewer-11.0.tar.xz"
+  sha256 "a43fa2325c4c1c77a5c8c98065ac30ef0511a21ac98e590f22340869bad9abd0"
 
+  depends_on "bash-completion@2" => :build
+  depends_on "gobject-introspection" => :build
   depends_on "intltool" => :build
-  depends_on "libtool" => :build
-  depends_on "pkg-config" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "shared-mime-info" => :build
+  depends_on "spice-protocol" => :build
 
-  depends_on "atk"
-  depends_on "cairo"
-  depends_on "gdk-pixbuf"
-  depends_on "gettext"
-  depends_on "glib"
-  depends_on "gtk+3"
   depends_on "gtk-vnc"
-  depends_on "hicolor-icon-theme"
   depends_on "libvirt"
   depends_on "libvirt-glib"
-  depends_on "pango"
   depends_on "shared-mime-info"
   depends_on "spice-gtk"
-  depends_on "spice-protocol"
+  depends_on "vte3"
 
   def install
-    args = %W[
-      --disable-silent-rules
-      --disable-update-mimedb
-      --with-gtk-vnc
-      --with-spice-gtk
-      --prefix=#{prefix}
-    ]
-    system "./configure", *args
-    system "make", "install"
+    mkdir "build" do
+      args = %w[
+        -Dlibvirt=enabled
+        -Dvnc=enabled
+        -Dspice=enabled
+        -Dovirt=disabled
+        -Dvte=enabled
+        -Dbash_completion=enabled
+      ]
+
+      system "meson", "..", *std_meson_args, *args
+      system "ninja"
+      system "ninja", "install"
+    end
   end
 
   def post_install
